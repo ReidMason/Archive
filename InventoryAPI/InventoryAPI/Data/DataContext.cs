d6159e8dfb18d930bd318f10838662aff1a8f4ca
Reid Mason <maddogshain132@gmail.com>
2020-03-03 15:25:12 +0000

    Add files via upload

---
+++ InventoryAPI/InventoryAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ InventoryAPI/InventoryAPI/Dtos/InventoryItem/GetInventoryItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Dtos.InventoryItem
+{
+    public class GetInventoryItemDto
+    {
+        // Remove properties you don't want displayed
+        public int? Id { get;  set; } = null;
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
