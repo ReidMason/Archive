7b7f12a3783c0990d6070cdf397afbef6e05b03a
Reid Mason <maddogshain123@gmail.com>
2021-05-09 01:56:07 +0100

    Changed popover to tippyjs

--- src/SPopover/SPopover.tsx
+++ src/SPopover/SPopover.tsx
@@ -1,33 +1,19 @@
-import React from 'react'
-import { usePopperTooltip } from "react-popper-tooltip";
+import React from 'react';
+import Tippy from '@tippyjs/react';
 
 interface SPopoverProps {
-    children: React.ReactNode;
+    children: React.ReactElement;
     text: string;
 }
 
 export default function SPopover({ children, text }: SPopoverProps) {
-    const {
-        getTooltipProps,
-        setTooltipRef,
-        setTriggerRef,
-        visible
-    } = usePopperTooltip({
-        placement: "top",
-        delayShow: 700
-    });
-
-
-    const opacityStyle = visible ? "opacity-100" : "opacity-0";
-
     return (
         <div className="mt-24">
-            <button type="button" ref={setTriggerRef}>{children}</button>
-            <div ref={setTooltipRef} {...getTooltipProps({ className: "tooltip-container" })}>
-                <div className={`p-1 px-2 text-snow-2 ${opacityStyle} shadow bg-night-4 rounded transition-all duration-300`}>
-                    {text}
+            <Tippy className="p-1 px-2 text-snow-2 shadow bg-night-4 rounded" content={text} animation="fade" delay={[700, 0]}>
+                <div className="inline-block">
+                    {children}
                 </div>
-            </div>
+            </Tippy>
         </div>
     )
 }
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import SButton from "./SButton";
 import SInput from "./SInput";
 import SPopover from "./SPopover";
 import "tailwindcss/tailwind.css";
+import "./main.scss";
 
 export { SButton, SInput, SPopover };
